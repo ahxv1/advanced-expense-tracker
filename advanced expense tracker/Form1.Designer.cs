94697b4607bb1c1b2799e7f8148d8432e85ca633
AHMAD ABAZAID <ahmadabazaidf@gmail.com>
2024-12-20 15:18:27 +0300

    first step user login

--- advanced expense tracker/Form1.Designer.cs
+++ advanced expense tracker/Form1.Designer.cs
@@ -28,12 +28,90 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            button1 = new Button();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            button2 = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(140, 285);
+            button1.Name = "button1";
+            button1.Size = new Size(103, 44);
+            button1.TabIndex = 0;
+            button1.Text = "Loh In";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(122, 82);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(171, 27);
+            textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(122, 146);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(171, 27);
+            textBox2.TabIndex = 2;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(294, 332);
+            button2.Name = "button2";
+            button2.Size = new Size(112, 33);
+            button2.TabIndex = 3;
+            button2.Text = "Delet the Text";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(35, 85);
+            label1.Name = "label1";
+            label1.Size = new Size(73, 20);
+            label1.TabIndex = 4;
+            label1.Text = "username";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(35, 153);
+            label2.Name = "label2";
+            label2.Size = new Size(72, 20);
+            label2.TabIndex = 5;
+            label2.Text = "password";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(432, 387);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(button2);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button button1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Button button2;
+        private Label label1;
+        private Label label2;
     }
 }
--- advanced expense tracker/Form1.cs
+++ advanced expense tracker/Form1.cs
@@ -6,5 +6,26 @@ namespace advanced_expense_tracker
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string user = textBox1.Text;
+            string pass = textBox2.Text;
+
+            if (user == "asd" && pass == "123")
+            {
+                MessageBox.Show("Welcome Admin");
+            }
+            else
+            {
+                MessageBox.Show("Error username or password");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+        }
     }
 }
